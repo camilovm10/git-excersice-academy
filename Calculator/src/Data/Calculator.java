88f21e0550a8ab91fd97dff102777fcc56b33f9c
Camilo Vasquez <camilovasquez9738@hotmail.com>
2021-01-21 12:48:32 -0500

    Sum and substract methods added to base_calculator

--- Calculator/src/Data/Calculator.java
+++ Calculator/src/Data/Calculator.java
@@ -1,4 +1,13 @@
 package Data;
 
 public class Calculator {
+
+    public int sumMethod(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int substractMethod(int num1, int num2) {
+        return num1 - num2;
+    }
+
 }
