30a8c703417b9cd0f95c586251613fbf42213c63
Camilo Vasquez <camilovasquez9738@hotmail.com>
2021-01-21 12:51:21 -0500

    multiply and divition methods added on advanced_calculator

--- Calculator/src/Data/Calculator.java
+++ Calculator/src/Data/Calculator.java
@@ -10,4 +10,12 @@ public class Calculator {
         return num1 - num2;
     }
 
+    public int multiplyMethod(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divitionMethod(int num1, int num2) {
+        return num1 / num2;
+    }
+
 }
